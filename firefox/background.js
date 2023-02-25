18ea240c6ec89d7a370550d1ce5b2f3f79694718
seiuneko <seiunekosl@gmail.com>
2023-02-25 21:26:07 +0800

    refactor(firefox): 事件监听器回调使用匿名函数

--- firefox/background.js
+++ firefox/background.js
@@ -1,40 +1,35 @@
-// modify from: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/webRequest/StreamFilter/ondata
-function replaceDomain(details) {
-    let filter = browser.webRequest.filterResponseData(details.requestId);
-    let decoder = new TextDecoder("utf-8");
-    let encoder = new TextEncoder();
-
-    const data = [];
-    filter.ondata = (event) => {
-        data.push(decoder.decode(event.data, {stream: true}));
-    };
-
-    filter.onstop = () => {
-        data.push(decoder.decode());
-
-        let str = data.join("");
-        str = str.replaceAll(/moyu\.im/g, 'sinaimg.cn');
-        filter.write(encoder.encode(str));
-        filter.close();
-    };
-}
-
-
-function addReferer(details) {
-    let headers = details.requestHeaders;
-    headers.push({name: "Referer", value: "https://weibo.com/"});
-
-    return {requestHeaders: headers};
-}
-
 browser.webRequest.onBeforeRequest.addListener(
-    replaceDomain,
+    (details) => {
+        // modify from: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/webRequest/StreamFilter/ondata
+        let filter = browser.webRequest.filterResponseData(details.requestId);
+        let decoder = new TextDecoder("utf-8");
+        let encoder = new TextEncoder();
+
+        const data = [];
+        filter.ondata = (event) => {
+            data.push(decoder.decode(event.data, {stream: true}));
+        };
+
+        filter.onstop = () => {
+            data.push(decoder.decode());
+
+            let str = data.join("");
+            str = str.replaceAll(/moyu\.im/g, 'sinaimg.cn');
+            filter.write(encoder.encode(str));
+            filter.close();
+        };
+    },
     {urls: ["*://jandan.net/*", "*://i.jandan.net/*"], types: ["main_frame"]},
     ["blocking"]
 );
 
 browser.webRequest.onBeforeSendHeaders.addListener(
-    addReferer,
+    (details) => {
+        let headers = details.requestHeaders;
+        headers.push({name: "Referer", value: "https://weibo.com/"});
+
+        return {requestHeaders: headers};
+    },
     {urls: ["*://*.sinaimg.cn/*"], types: ["image", "main_frame"]},
     ["blocking", "requestHeaders"]
 );
